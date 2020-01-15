c72165bb55257ef897fbf99add7fd2e6f92acb52
Mitchell-Johnson <mitchj9@uw.edu>
2020-01-15 13:26:17 -0800

    working url gen

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 var apiKey = "DQ99xseSmJfhFUjox4O0vQvEKtMMl9M2";
-var keyWord = "";
+var keyWord = "weather";
 var limit = 0;
-var startDate = "";
+var startDate = "2000";
 var endDate = "";
 
-var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22${keyWord}%22&api-key=${apiKey}`;
+if(startDate != ""){
+    var startDateParam = `&begin_date=${startDate + "0101"}`;
+} else {var startDateParam = "";}
+
+if(endDate != ""){
+    var endDateParam = `&end_date=${endDate + "0101"}`;
+} else {
+    var endDateParam = "";
+}
+
+var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22${keyWord}%22${startDateParam}${endDateParam}&fl=web_url&fl=lead_paragraph&api-key=${apiKey}`;
 
 //find out how to use the startDate and endDate
+console.log(url);
