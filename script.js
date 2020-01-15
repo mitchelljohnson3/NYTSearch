cee20befea9053ab9c9e498edfb55c63c3d6fdae
Mitchell-Johnson <mitchj9@uw.edu>
2020-01-15 12:35:46 -0800

    update

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-$("#submit").on("click", function() {
-    $("#content").text("This works!");
-    
-    });
+var apiKey = "DC6Gaxb9OrpUuqq3HHVItGoekb60UPhr";
+var keyWord = "";
+var limit = 0;
+var startDate = "";
+var endDate = "";
+
+var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22${keyWord}%22&api-key=${apiKey}`;
+
+//find out how to use the startDate and endDate
