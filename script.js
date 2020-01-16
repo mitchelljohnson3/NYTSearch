2148ff7f4f17bea52061cc708394f92f1dcb6b15
Mitchell-Johnson <mitchj9@uw.edu>
2020-01-15 23:27:47 -0800

    finished

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiKey = "DQ99xseSmJfhFUjox4O0vQvEKtMMl9M2";
 
-let genUrl = (keyWord, limit, startDate, endDate) => {
+let genUrl = (keyWord, startDate, endDate) => {
     if(startDate != ""){
         var startDateParam = `&begin_date=${startDate + "0101"}`;
     } else {var startDateParam = "";}
@@ -10,7 +10,7 @@ let genUrl = (keyWord, limit, startDate, endDate) => {
     } else {
         var endDateParam = "";
     }
-    return `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22${keyWord}%22${startDateParam}${endDateParam}&fl=web_url&fl=lead_paragraph&api-key=${apiKey}`;
+    return `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=%22${keyWord}%22${startDateParam}${endDateParam}&fl=web_url&fl=lead_paragraph&fl=pub_date&api-key=${apiKey}`;
 };
 
 let showError = () => {
@@ -19,16 +19,19 @@ let showError = () => {
 
 var button = document.getElementById("submit");
 var term = document.getElementById("term");
-var limit = document.getElementById("limit");
 var start = document.getElementById("start");
 var end = document.getElementById("end");
+var limit = document.getElementById("limit");
+var contentDiv = document.getElementById("content-div");
+
 button.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
     let goodSearch = true;
     if(term.value === "" || limit.value === ""){
         goodSearch = false;
     }
     if(goodSearch){
-        let url = genUrl(term.value, limit.value, start.value, end.value);
+        let url = genUrl(term.value, start.value, end.value);
         fetch(url).then((response) => {
             return response.json();
         }).then((json) => {
@@ -40,5 +43,23 @@ button.addEventListener("click", () => {
 });
 
 let displayArticles = (jsonObject) => {
-    
+    var articles = jsonObject.response.docs;
+    var lim = limit.value;
+    for(let i = 0; i < lim; i++){
+        let newDiv = document.createElement("div");
+        let title = document.createElement("h2");
+        let pubDate = document.createElement("p");
+        let url = document.createElement("a");
+
+        title.textContent = articles[i].lead_paragraph;
+        pubDate.textContent = "Publish date: " + articles[i].pub_date.split("T")[0];
+        url.setAttribute("href", articles[i].web_url);
+        url.textContent = articles[i].web_url;
+        newDiv.appendChild(title);
+        newDiv.appendChild(pubDate);
+        newDiv.appendChild(url);
+        newDiv.setAttribute("style", "border: 1px solid black; margin-bottom: 10px");
+
+        contentDiv.appendChild(newDiv);
+    }
 }
